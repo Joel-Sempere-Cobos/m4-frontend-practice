ae8e56de43231562db83fb5c7fd2d732c23444a4
Joel-Sempere-Cobos <91223315+Joel-Sempere-Cobos@users.noreply.github.com>
2022-10-17 00:02:05 +0200

    Tamaño imagen en CSS

--- ad-details/adDetailsView.js
+++ ad-details/adDetailsView.js
@@ -1,15 +1,13 @@
 export const buildAdDetails = (ad) => {
-    //TODO ajustar tamaño imagen por css y no html
     const adView = `
     <article class="ad">
-    <img src="${ad.img}"  width="100px" >
+    <img src="${ad.img}">
     <p>${ad.title}</p>
     <p>${ad.description}</p>
     <p>Precio: ${ad.price}€</p>
     <p>Anunciante: @${ad.user.username}</p>
     <button style="display: none">Borrar anuncio</button>
     </article>
-    
     `;
 
     return adView;
--- ads-list/adsListView.js
+++ ads-list/adsListView.js
@@ -1,12 +1,10 @@
 export const buildAdsListView = (ad) => {
-    //TODO ajustar tamaño imagen por css y no html
     const adView = `
-    <img src="${ad.img}"  width="100px" >
+    <img src="${ad.img}">
     <p>${ad.title}</p>
     <p>${ad.description}</p>
     <p>Precio: ${ad.price}€</p>
     `;
-
     return adView;
 };
 
